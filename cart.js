31eee0e90b84361a9ab8abb886af012ea9c3fda0
AlexBolocan <100614307+AlexBolocan@users.noreply.github.com>
2022-07-01 23:58:30 +0300

    update cart

--- cart.js
+++ cart.js
@@ -14,6 +14,7 @@ let dataBaseList = {};
 function getCart() {
   let stringHtml = "";
   let cart = localStorage.getItem("cart");
+  // desenare html mesaj cos gol
   if (cart === null || cart.length < 3) {
     stringHtml += `
     <div class="card mb-3 h-40">
@@ -24,89 +25,77 @@ function getCart() {
             </div>
        </div>
      </div>
-
 `;
     document.querySelector("#cartContainer").innerHTML = stringHtml;
   } else {
     totalPriceCart = 0;
     totalProductCart = 0;
     cart = JSON.parse(cart);
+    // desenare html pentru total produse si cost
     for (let i = 0; i < cart.length; i++) {
-      //checkAvailability(i);
-      valueProductCart[i] = cart[i].pret * cart[i].cantitate;
-      totalPriceCart += valueProductCart[i];
-      totalProductCart += cart[i].cantitate;
+      // checkAvailability(i);
+      valueProductCart = Number(cart[i].pret) * Number(cart[i].cantitate);
+      totalPriceCart += valueProductCart;
+      totalProductCart += Number(cart[i].cantitate);
     }
-
     stringHtml += `
-    <div class="card mb-2 h-20">
+    <div class="card mb-2 h-40">
       <div class="d-md-flex ">
-         <div class="col-md-2"justify-content-md-start >
-          <span><h5 class=" card-text">Produse: ${totalProductCart} buc</h5></span>
-          <span><h5 class=" card-text">Cost : ${totalPriceCart} RON</h5></span>
+         <div class="col-md-5 justify-content-md-start" >
+          <h5 class=" card-text text-center">Total: ${totalProductCart} produse</h5>
+          </div>
+          <div class="col-md-5 justify-content-md-start" >
+          <h5 class=" card-text text-center">Cost total : ${totalPriceCart} RON</h5>
          </div>
-         <div class="col-md-2"justify-content-md-end >
-          <button onclick="" class="text-end btn btn-outline-secondary " type="button">Cumpara </button>
+         <div class="h-100 col-md-2 justify-content-end text-end" >
+          <button onclick="buyProducts()" class="text-end btn btn-outline-success" type="button">Cumpara  
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag" viewBox="0 0 16 16">
+          <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z"/>
+        </svg></button>
        </div>
       </div>  
     </div>
  `;
+    // draw html with details of product
     for (let i = 0; i < cart.length; i++) {
-      //checkAvailability(i);
-      valueProductCart[i] = cart[i].pret * cart[i].cantitate;
       stringHtml += ` 
-          <div class="row g-0 text-end">
-            <div class="d-md-flex  justify-content-end">
-             <div class="input-group-text " ><a href="details.html?id=${cart[i].idProdus}"> ${cart[i].title}</a></div>
-              <span class="input-group-text">Pret: ${cart[i].pret} RON</span>
+          <div class="card p-2 mb-2 justify-content-evenly text-end">
+            <div class=" d-md-flex justify-content-end" style="max-width: 100rem;">
+             <div class="input-group-text bg-light text-dark" ><a style="width:350px"; href="details.html?id=${
+               cart[i].idProdus
+             }"> ${cart[i].title}</a></div>
+              <span class="input-group-text bg-light text-dark justify-content-end "style="min-width: 100px;">Pret: ${
+                cart[i].pret
+              } RON</span>
+              <span class="input-group-text bg-light text-dark justify-content-end">
                 <button onclick="decrProductToCart(${i})" class=" btn btn-outline-secondary" type="button">-</button>
-                <input onchange="" type="number" class="form-control" style="width:100px;" "name="quantity" value="${cart[i].cantitate}">
+                <input id="idQuantity" onchange="checkValue(${i})" type="number" class="form-control text-end" style="width:100px;" name="quantity" value="${
+        cart[i].cantitate
+      }">
                 <button onclick="incrProductToCart(${i})" class="btn btn-outline-secondary" type="button">+</button>
                <span class="input-group-text">BUC</span>  
-              <span class="input-group-text">TOTAL: ${valueProductCart[i]} RON</span>
-             <button onclick="deleteProduct(${i})" class="btn btn-outline-secondary" type="button" id="button-addon2" >Sterge 
+             </span>
+              <span class="input-group-text bg-light text-dark justify-content-end " style="min-width:350px;">TOTAL: ${
+                Number(cart[i].pret) * Number(cart[i].cantitate)
+              } RON</span>
+             <button onclick="deleteProduct(${i})" class="btn btn-outline-danger" type="button" id="button-addon2" >Sterge 
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16" >
                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
                <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
               </svg>
             </button>
-          </div>
+           </div>
           </div>
           `;
-      totalPriceCart += valueProductCart[i];
-      totalProductCart += cart[i].cantitate;
+      if (cart !== null) {
+        checkStockAvailability(i);
+      }
     }
     //modal pentru confirmarea stocului
-    stringHtml+=`
-    <!-- Modal stoc -->
-    <div class="modal fade" id="staticBackdrop" name="stocModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-      <div class="modal-dialog  ">
-        <div class="modal-content bg-danger text-white">
-          <div class="modal-header">
-          <h5 class="modal-title" id="staticBackdropLabel">Stoc depasit</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-          <div class="modal-body">
-          <h5 class="modal-title bg-danger text-white">Stocul de produse a fost setat la valoarea maxima:  buc  </h5>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-primary">Am inteles</button>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    `;  
+    stringHtml += `
+    `;
     document.querySelector("#cartContainer").innerHTML = stringHtml;
   }
-
-}
-
-// functia ce actualizeaza localStorage
-function updateQuantity(idx) {
-  let cartQuantity = [];
-  cartQuantity = document.querySelectorAll("[name='prodQuantity']");
-  console.log(cartQuantity[idx]);
 }
 
 // functia care sterge un produs din local storage
@@ -128,7 +117,7 @@ function incrProductToCart(idx) {
   cart = JSON.parse(cart);
   cart[idx].cantitate++;
   localStorage.setItem("cart", JSON.stringify(cart));
-  checkAvailability(idx);
+  checkStockAvailability(idx);
   getCart();
 }
 
@@ -138,29 +127,93 @@ function decrProductToCart(idx) {
   cart = JSON.parse(cart);
   cart[idx].cantitate--;
   localStorage.setItem("cart", JSON.stringify(cart));
-  checkAvailability(idx);
+  checkStockAvailability(idx);
   getCart();
 }
 
 // functia de verificare produs
-async function checkAvailability(idx) {
-  const myStocModal = new bootstrap.Modal(document.getElementById('staticBackdrop'))
+async function checkStockAvailability(idx) {
+  console.log("am intrat pe checkStock");
+  let productFound = false;
+  var myModal = new bootstrap.Modal(document.getElementById("exampleModal"));
   cart = localStorage.getItem("cart");
   cart = JSON.parse(cart);
-  const response = await fetch(dataBaseUrl + cart[idx].idProdus + "/.json");
-  dataBaseList = await response.json();
-  if (dataBaseList === null) {
-    state.product = {};
+  const resAllProduct = await fetch(dataBaseUrl + ".json");
+  dataBaseProducts = await resAllProduct.json();
+  if (dataBaseProducts === null) {
+    window.location = "index.html";
   } else {
-    state.product = dataBaseList;
-  }
-  quantityCart =Number( document.querySelector("[name='quantity']").value.trim());
+    for (let nrIdProdus of Object.keys(dataBaseProducts)) {
+      if (cart[idx].idProdus === nrIdProdus) {
+        productFound = true;
+        break;
+      } else {
+        productFound = false;
+      }
+    }
+    // check product available
+    if (productFound === false) {
+      let stringHtml = `
+    <p class="fs-3"> Cosul a fost actualizat. Unele produse nu mai sunt disponibile</p>
+    `;
+      document.querySelector("#textModal").innerHTML = stringHtml;
+      myModal.show();
+      deleteProduct(idx);
+    } else {
+      const response = await fetch(dataBaseUrl + cart[idx].idProdus + "/.json");
+      dataBaseList = await response.json();
 
-  if (Number(state.product.stock) < cart[idx].cantitate) {
-    myStocModal.show();
-    cart[idx].cantitate = Number(state.product.stock);
-    localStorage.setItem("cart", JSON.stringify(cart));
-    getCart();
+      if (dataBaseList === null) {
+        state.product = {};
+      } else {
+        state.product = dataBaseList;
+      }
+      // check maxim stock
+      if (Number(state.product.stock) < cart[idx].cantitate) {
+        let stringHtml = `
+    <p class="fs-3"> Cantiatea produselor din cos a fost actaulizat la valoarea maxima a stocului</p>
+    `;
+        document.querySelector("#textModal").innerHTML = stringHtml;
+        myModal.show();
+        cart[idx].cantitate = Number(state.product.stock);
+        localStorage.setItem("cart", JSON.stringify(cart));
+        getCart();
+      }
+
+    if (Number(state.product.price) !== Number(cart[idx].pret)) {
+        let stringHtml = `
+    <p class="fs-3"> Pretul produselor din cos a fost actaulizat.</p>
+    `;
+        document.querySelector("#textModal").innerHTML = stringHtml;
+        myModal.show();
+        cart[idx].pret = Number(state.product.price);
+        localStorage.setItem("cart", JSON.stringify(cart));
+        getCart();
+      }
+
+      // check minim stock
+      if (0 >= cart[idx].cantitate) {
+        cart[idx].cantitate = 1;
+        localStorage.setItem("cart", JSON.stringify(cart));
+        getCart();
+      }
+    }
   }
-  
 }
+
+// update local storage wiht input value
+function checkValue(idx) {
+  cart = localStorage.getItem("cart");
+  cart = JSON.parse(cart);
+  let valueStocInput = [];
+  let varNr = document.querySelectorAll("[name='quantity']")[idx];
+  valueStocInput = varNr.value;
+  if (Number(valueStocInput) !== cart[idx].cantitate) {
+    cart[idx].cantitate = valueStocInput;
+  }
+  localStorage.setItem("cart", JSON.stringify(cart));
+  getCart();
+}
+
+// functia de finalizare comanda => sterge locat storage si stocul produselor
+function buyProducts() {}
--- details.js
+++ details.js
@@ -11,7 +11,6 @@ if (index.length < 1) {
 async function getDetailProduct() {
   const response = await fetch(dataBaseUrl + index + "/.json");
   dataBaseProduct = await response.json();
-  console.log(dataBaseProduct);
   detailsDraw();
   document.querySelector("#addToCartAlert").classList.add("fade");
   document.querySelector("#addToCartAlert").classList.remove("show");
@@ -47,7 +46,7 @@ function detailsDraw() {
           <span class="visually-hidden">Next</span>
        </button>
     </div>
-    <div class="col">
+    <div class="col ">
         <div> 
          <h1 class="display-6">${dataBaseProduct.description}</h1></div>
          <hr>
@@ -57,7 +56,7 @@ function detailsDraw() {
          </div> 
          <div class="input-group mb-8">
           <input type="number" onchange="quantityChange()" class="form-control" placeholder="Stock" aria-label="Add part on stock" aria-describedby="button-addon2" name="cantitate" value="1">
-          <button onclick="addProductCart()" class="btn btn-outline-secondary" type="button" id="button-addon2" name="addToCartButton">Adauga in cos
+          <button onclick="addProductCart()" class="btn btn-outline-success" type="button" id="button-addon2" name="addToCartButton">Adauga in cos
            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart3" viewBox="0 0 20 20">
             <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
            </svg>
@@ -91,13 +90,11 @@ function addProductCart() {
   };
 
   let cart = localStorage.getItem("cart");
-  console.log(cart);
   if (cart === null) {
     cart = [];
   } else {
     cart = JSON.parse(cart);
   }
-  console.log(cart);
 
   let found = false;
   for (let p of cart) {
